8c70841c1cb3ebb0389aaf6ada75c8cffc15e2db
daud <daudahmed@zoho.com>
2020-05-17 09:22:43 -0400

    finalizer fileparser 1

--- fileparser/scripts/main.py
+++ fileparser/scripts/main.py
@@ -14,23 +14,17 @@ def main(folder_path=None):
 
     if folder_path == None:
         return
-
     updateFSRStatus(folder_path, True)
-
     fsi_object = FolderSelectInfoModel.objects.get(folder_path__exact=folder_path)
     if fsi_object == None:
         updateFSRStatus(folder_path, False)
         return
-    print(fsi_object)
-    print(fsi_object.folder_path)
     fri_object = FileReadInfoModel.objects.get(folder=fsi_object)
     if fri_object == None:
         updateFSRStatus(folder_path, False)
         return
-    # here we have to add the logic for selecting the appropriate parser based on parser value
     f = FileExtractor(fsi_object.folder_path, fsi_object.extensions)
     additional_file = fsi_object.additional_file_path
-    # additional_file = "/daud/Desktop/additonalfile.txt"
     additional_field = fsi_object.additional_fields
     # additonal_regex = r"^.*  (.*)\..*$"
     additional_regex = fsi_object.regex_for_additional_file_path
@@ -77,17 +71,18 @@ def subMain(f, fri, lines=None, additonal_regex=None, additional_field=None):
                     groups = pattren.match(line)
                 else:
                     groups = None
-
+            print("this is where you should zoom in lol")
+            print(fri.folder.parser)
+            Parser = returnRightParser(fri.folder.parser)
             if groups:
                 groups = list(groups.groups())
                 if len(groups) == len(additional_field):
-                    p = TextFileParser(each, fri.length, fri.order, fri.delimeter, fri.start_line, groups, additional_field)
+                    p = Parser(each, fri.length, fri.order, fri.delimeter, fri.start_line, groups, additional_field)
                 else:
-                    p = TextFileParser(each, fri.length, fri.order, fri.delimeter, fri.start_line)
+                    p = Parser(each, fri.length, fri.order, fri.delimeter, fri.start_line)
                 p.readFile()
             else:
-                print("wao")
-                p = TextFileParser(each, fri.length, fri.order, fri.delimeter, fri.start_line)
+                p = Parser(each, fri.length, fri.order, fri.delimeter, fri.start_line)
                 p.readFile()
 
         except Exception as e:
@@ -102,3 +97,20 @@ def updateFSRStatus(folder_path, status):
     except Exception as e:
         print(e)
         print('exception in updating FSR instance')
+
+def returnRightParser(parser):
+    '''
+    The method for returning the instance of right Parser given the parser name
+
+    :param parser: The name of parser
+    :type parser: str
+    '''
+    if parser == None or parser == "":
+        return
+    parserDict = {
+        "TextFileParser": TextFileParser
+    }
+    if parser in parserDict:
+        return parserDict[parser]
+    else:
+        return None
